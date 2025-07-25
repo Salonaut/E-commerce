7c3fe196e8c068d6ba895e13b2211382be379563
Hryhorii Dovhaliuk <dgrigorij77@gmail.com>
2025-07-25 10:51:59 +0300

    Create app main and add models

---
+++ ecommerce/main/models.py
@@ -0,0 +1,69 @@
+from tkinter.constants import CASCADE
+
+from django.db import models
+from django.utils.text import slugify
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.CharField(max_length=100, unique=True)
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name) # name = Nike Air Force => nike-air-force
+        super().save(*args, **kwargs)
+
+
+
+    def __str__(self):
+        return self.name
+
+
+class Size(models.Model):
+    name = models.CharField(max_length=75)
+
+
+    def __str__(self):
+        return self.name
+
+
+class ProductSize(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, related_name='product_size')
+    size = models.ForeignKey(Size, on_delete=CASCADE)
+    stock = models.PositiveIntegerField(default=0)
+
+
+    def __str__(self):
+        return f"{self.size.name} ({self.stock}) for {self.product.name}"
+
+
+class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    slug = models.CharField(max_length=100, unique=True)
+    color = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    main_image = models.ImageField(upload_to='products/main/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
+
+
+
+    def __str__(self):
+        return self.name
+
+
+
+class ProductImage(models.Model):
+    product = models.ForeignKey(Product, on_delete=CASCADE, related_name='images')
+    image = models.ImageField(upload_to='products/extra/')
+
