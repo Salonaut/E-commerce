7d5f04d779fa8ac9f7f97b487c6c031925f974f3
Hryhorii Dovhaliuk <dgrigorij77@gmail.com>
2025-08-05 19:16:48 +0300

    Start create models cart

--- ecommerce/cart/models.py
+++ ecommerce/cart/models.py
@@ -6,5 +6,15 @@ from decimal import Decimal
 
 class Cart(models.Model):
     session_key = models.CharField(max_length=40, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
 
+
+    def __str__(self):
+        return f'Cart {self.session_key}'
+
+
+    @property
+    def total_items(self):
+        pass
 
