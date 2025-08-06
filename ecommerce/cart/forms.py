0c575c18de6ac3947df92ec7971e812a94288b53
Hryhorii Dovhaliuk <dgrigorij77@gmail.com>
2025-08-06 14:38:28 +0300

    Create models and form for card

---
+++ ecommerce/cart/forms.py
@@ -0,0 +1,50 @@
+from django import forms
+from .models import CartItem
+
+
+
+
+class AddToCartForm(forms.Form):
+    size_id = forms.IntegerField(required=False)
+    quantity = forms.IntegerField(min_value=1, initial=1)
+    
+    def __init__(self, *args, product=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.product = product
+
+
+        if product:
+            sizes = product.product_sizes.filter(stock__gt=0)
+            if sizes.exists():
+                self.fields['size_id'] = forms.ChoiceField(
+                    choices=[(ps.id, ps.size.name) for ps in sizes],
+                    required=False,
+                    initial=sizes.first().id
+                )
+
+
+class UpdateCartForm(forms.ModelForm):
+    class Meta:
+        model = CartItem
+        fields = ['quantity']
+
+
+    def __init__(self, *args, **kwargs):
+       super().__init__(*args, **kwargs)
+       if self.instance and self.instance.product_sizes:
+           self.fields['quality'].validators.append(
+               forms.validators.MaxValueValidator(self.instance.product_sizes.stock)
+           )
+
+
+
+
+
+
+
+
+
+
+
+
+
