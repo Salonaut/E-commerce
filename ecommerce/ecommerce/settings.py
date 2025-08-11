2bdb91ba1bfc44c2175f555050058bb0d13f70d7
Hryhorii <dgrigorij77@gmail.com>
2025-08-11 15:11:57 +0300

    Create models for orders

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'main',
     'cart',
     'users',
+    'orders'
 
 ]
 
---
+++ ecommerce/orders/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from django.utils.html import strip_tags
+
+
+class OrderCreationForm(forms.Form):
+    pass
